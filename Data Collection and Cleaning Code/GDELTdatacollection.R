fa84fb89725e0319fb6cf137f63e3e570cadae75
Faiz Essa <faiz.essa@gmail.com>
2023-03-08 17:04:18 -0500

    Added Indian Stata Raw GDELT data
    Removed district GDELT data and added state data.

--- Data Collection and Cleaning Code/GDELTdatacollection.R
+++ Data Collection and Cleaning Code/GDELTdatacollection.R
@@ -1,6 +1,6 @@
 # Faiz Essa
 # GDELT Data Collection
-# 01/26/2023
+# 03/08/2023
 
 # importing packages
 library(tidyverse)
@@ -10,51 +10,31 @@ library(bigrquery)
 bq_auth(email = "faiz.essa@gmail.com")
 billing <- "whatsappgdelt"
 
-
-# COUNTRIES
-# We would like to construct a panel of collective action events daily across
-# COUNTRIES We will focus on ``protest" events.
-# SQL query
-sql <- 
-  "SELECT ActionGeo_CountryCode, SQLDATE, EventCode, COUNT(*) 
-FROM `gdelt-bq.gdeltv2.events` 
-WHERE EventRootCode = '14' 
-AND Year >= 2013
-GROUP BY ActionGeo_CountryCode, SQLDATE, EventCode
-"
-# data import
-gdelt.data <- bq_project_query(billing, sql)
-gdelt.table <- bq_table_download(gdelt.data)
-
-# 21.73 gb, 1TB limit per month  (for free)
-# w/ the last query 2(21.73 gb have been used total)
-
-# save raw data
-write_rds(gdelt.table, "Data/GDELT/raw_gdelt_data_country.rds")
-
-rm(list=ls())
-
 # INDIA 
-billing <- "whatsappgdelt"
 # We would like to construct a panel of collective action events daily across
 # indian districts. We will focus on ``protest" events.
 # SQL query
 sql <- 
-  "SELECT ActionGeo_ADM2Code, SQLDATE, EventCode, COUNT(*) 
+  "SELECT ActionGeo_ADM1Code, SQLDATE, EventCode, COUNT(*) 
 FROM `gdelt-bq.gdeltv2.events` 
 WHERE EventRootCode = '14' 
 AND ActionGeo_CountryCode = 'IN'
-AND Year >= 2013
-GROUP BY ActionGeo_ADM2Code, SQLDATE, EventCode
+AND Year >= 2015
+GROUP BY ActionGeo_ADM1Code, SQLDATE, EventCode
 "
 
 # data import
 gdelt.data <- bq_project_query(billing, sql)
 gdelt.table <- bq_table_download(gdelt.data)
 
-# 21.73 gb, 1TB limit per month  (for free)
+# 23.11 gb, 1TB limit per month  (for free)
 
 # save raw data
-write_rds(gdelt.table, "Data/GDELT/raw_gdelt_data.rds")
+write_rds(gdelt.table, "Data/GDELT/RawGDELT_IndiaStates.rds")
+
+# clear data 
+rm(list=ls())
+
+
 
 
